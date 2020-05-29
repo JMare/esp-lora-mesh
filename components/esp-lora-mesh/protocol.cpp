dd2c973202d9f8d83295d756abe4f3319f07f840
James Mare <jamesmare12@gmail.com>
2020-05-29 14:05:04 +1000

    Wrote a practical encapulated header/msg system to allow multiple msg_ids

--- components/esp-lora-mesh/protocol.cpp
+++ components/esp-lora-mesh/protocol.cpp
@@ -1,2 +1,32 @@
 #include <protocol.h>
 
+int Packet::pack(uint8_t *buf)
+{
+  // Pack the header
+  buf[0] = source_id;
+  buf[1] = dest_id;
+  buf[2] = ttl;
+  buf[3] = msg_id;
+
+  // call msg.pack to fill the rest of the buffer
+  int msg_len = this->msg.pack(&buf,PACKET_HEADER_SIZE);
+
+  // Return total length of header + message
+  return msg_len + PACKET_HEADER_SIZE;
+}
+
+bool Packet::unpack(uint8_t *buf)
+{
+  source_id = buf[0];
+  dest_id = buf[1];
+  ttl = buf[2];
+  msg_id = buf[3];
+
+  switch(msg_id)
+  {
+  case MSG_ID_SYNC:
+    SyncMessage tmp_msg;
+    tmp_msg.unpack(&buf,PACKET_HEADER_SIZE);
+    this->msg = tmp_msg;
+  }
+}
--- components/esp-lora-mesh/protocol.h
+++ components/esp-lora-mesh/protocol.h
@@ -4,52 +4,60 @@
 #include <stdint.h>
 
 #define LORA_MAX_PACKET_SIZE 256
-#define PACKET_HEADER_SIZE 2
+#define PACKET_HEADER_SIZE 4
 #define LORA_MAX_MESSAGE_LEN LORA_MAX_PACKET_SIZE-PACKET_HEADER_SIZE
+
+#define MSG_ID_SYNC 0x00
+#define MSG_ID_HBT 0x01
+
+// contains source and dest for entire trip
+class Packet {
+  uint8_t source_id;
+  uint8_t dest_id;
+  uint8_t ttl;
+  uint8_t msg_id;
+  Message msg;
+
+  int pack(uint8_t *buf);
+  void unpack(uint8_t *buf);
+};
 
 // Virtual class for message
 class Message {
  public:
-  virtual int pack(uint8_t *buf);
-  virtual void unpack(uint8_t *buf);
+  virtual int pack(uint8_t *buf, int index);
+  virtual void unpack(uint8_t *buf, int index);
 };
 
 class SyncMessage : Message {
  public:
-  int pack(uint8_t *buf)
+  int pack(uint8_t *buf, int index)
   {
     uint8_t byte0 = ((micros_to_slot_end >> 24) & 0xFF) ;
     uint8_t byte1 = ((micros_to_slot_end >> 16) & 0xFF) ;
     uint8_t byte2 = ((micros_to_slot_end >> 8 ) & 0XFF);
     uint8_t byte3 = (micros_to_slot_end & 0XFF);
 
-    buf[0] = this->slot_number;
-    buf[1] = byte0;
-    buf[2] = byte1;
-    buf[3] = byte2;
-    buf[4] = byte3;
+    buf[index] = this->slot_number;
+    buf[index + 1] = byte0;
+    buf[index + 2] = byte1;
+    buf[index + 3] = byte2;
+    buf[index + 4] = byte3;
 
     return 5;
   };
-  void unpack(uint8_t *buf)
-  {
-    this->slot_number = buf[0];
-    micros_to_slot_end = buf[4];
-    micros_to_slot_end = micros_to_slot_end | (buf[3] << 8);
-    micros_to_slot_end = micros_to_slot_end | (buf[2] << 16);
-    micros_to_slot_end = micros_to_slot_end | (buf[1] << 24);
-  }
 
-  const uint8_t msg_id = 0;
+  void unpack(uint8_t *buf, int index)
+  {
+    this->slot_number = buf[index];
+    micros_to_slot_end = buf[index + 4];
+    micros_to_slot_end = micros_to_slot_end | (buf[index + 3] << 8);
+    micros_to_slot_end = micros_to_slot_end | (buf[index + 2] << 16);
+    micros_to_slot_end = micros_to_slot_end | (buf[index + 1] << 24);
+  }
 
   uint8_t slot_number;
   uint32_t micros_to_slot_end;
-};
-
-struct Packet {
-  uint8_t source_id;
-  uint8_t msg_id;
-  Message msg;
 };
 
 #endif
